aeba1fc06c77de4d1f5e173f257fdaef5f130370
Alex Olsen <aolsenjazz@gmail.com>
2019-05-25 09:52:02 -0400

    disabling preventDefault on zone click

--- src/client/component/ParkingZone/index.js
+++ src/client/component/ParkingZone/index.js
@@ -11,8 +11,7 @@ export default class ParkingZone extends React.Component {
 	}
 
 	onClick(e) {
-		console.log('sanity');
-		e.preventDefault();
+		// e.preventDefault();
 		this.props.onClick();
 	}
 
