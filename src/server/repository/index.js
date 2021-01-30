3c9f86240f72bfce257d823d01d32029425ca59e
Alex Olsen <aolsenjazz@gmail.com>
2021-01-30 11:32:31 -0500

    migrate old project to old dir

--- src/server/repository/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	Feedback: require('./feedback-repo.js'),
-	Proposals: require('./proposal-repo.js'),
-	Corrections: require('./correction-repo.js'),
-	Zones: require('./zone-repo.js'),
-}
--- src/server/routes/404-router.js
+++
@@ -1,5 +0,0 @@
-function route(req, res, next) {
-	res.status(404).json({error: 'not found'});
-}
-
-export default route;
